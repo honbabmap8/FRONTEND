8a0bdba9c1c8ea66b5243686ccfb469ed2829582
임수연 <limsoo0816@naver.com>
2026-06-17 22:43:10 +0900

    feat: Eatbti result 배경 구현

--- honbabmap/src/data/storeData.js
+++ honbabmap/src/data/storeData.js
@@ -1,202 +1,207 @@
-
 export const storeData = [
   {
-  "message": "식당 상세 정보 조회에 성공했습니다.",
-  "data": [
-    {
-      "restaurantId": 1,
-      "name": "프랭크 버거 강남점",
-      "imageUrl": "https://example.com/images/burger_detail.jpg",
-      "restSoloLevel": 1,
-      "representativeMenu": "치즈버거 세트, 베이컨 치즈버거 세트",
-      "restaurantDetail": "혼밥하기 좋은 패스트푸드점",
-      "locationInfo": {
-        "stationId": 1,
-        "stationName": "강남역",
-        "distance": 100,
-        "time": 1
+    message: "안암역 기준 식당 상세 정보 조회에 성공했습니다.",
+    data: [
+      {
+        restaurantId: 1,
+        name: "안암 혼밥버거",
+        lat: 37.58653,
+        lng: 127.02918,
+        imageUrl: "https://example.com/images/burger_detail.jpg",
+        restSoloLevel: 1,
+        representativeMenu: "치즈버거 세트, 베이컨 치즈버거",
+        restaurantDetail: "안암역 근처에서 빠르게 혼밥하기 좋은 버거집",
+        locationInfo: {
+          stationId: 639,
+          stationName: "안암역",
+          distance: 35,
+          time: 1,
+        },
+        restFeatureList: [
+          {
+            featId: 1,
+            featName: "1인석 있음",
+          },
+          {
+            featId: 2,
+            featName: "1인 메뉴 가능",
+          },
+        ],
+        restReviewTagList: [
+          {
+            tagId: 1,
+            tagName: "가성비",
+          },
+          {
+            tagId: 2,
+            tagName: "빠른 식사",
+          },
+          {
+            tagId: 3,
+            tagName: "혼밥 추천",
+          },
+        ],
       },
-      "restFeatureList": [
-        {
-          "featId": 1,
-          "featName": "1인석 있음"
+      {
+        restaurantId: 2,
+        name: "참살이 우동",
+        lat: 37.58612,
+        lng: 127.02965,
+        imageUrl: "https://example.com/images/udon_detail.jpg",
+        restSoloLevel: 1,
+        representativeMenu: "가케우동, 돈가스 덮밥",
+        restaurantDetail: "혼자 앉기 편하고 회전이 빠른 안암역 우동집",
+        locationInfo: {
+          stationId: 639,
+          stationName: "안암역",
+          distance: 55,
+          time: 1,
         },
-        {
-          "featId": 2,
-          "featName": "1인 메뉴 가능"
-        }
-      ],
-      "restReviewTagList": [
-        {
-          "tagId": 1,
-          "tagName": "가성비"
-        },
-        {
-          "tagId": 2,
-          "tagName": "빠른 식사"
-        },
-        {
-          "tagId": 3,
-          "tagName": "야식 가능"
-        },
-        {
-          "tagId": 4,
-          "tagName": "웨이팅 적음"
-        }
-      ]
-    },
-    {
-      "restaurantId": 2,
-      "name": "역전우동 강남점",
-      "imageUrl": "https://example.com/images/udon_detail.jpg",
-      "restSoloLevel": 1,
-      "representativeMenu": "어묵우동, 돈까스",
-      "restaurantDetail": "혼자 빠르게 식사하기 좋은 우동 전문점",
-      "locationInfo": {
-        "stationId": 1,
-        "stationName": "강남역",
-        "distance": 250,
-        "time": 3
+        restFeatureList: [
+          {
+            featId: 1,
+            featName: "1인석 있음",
+          },
+          {
+            featId: 3,
+            featName: "키오스크 주문",
+          },
+        ],
+        restReviewTagList: [
+          {
+            tagId: 1,
+            tagName: "가성비",
+          },
+          {
+            tagId: 2,
+            tagName: "빠른 식사",
+          },
+          {
+            tagId: 5,
+            tagName: "따뜻한 메뉴",
+          },
+        ],
       },
-      "restFeatureList": [
-        {
-          "featId": 1,
-          "featName": "1인석 있음"
+      {
+        restaurantId: 3,
+        name: "고대앞 중식당",
+        lat: 37.58574,
+        lng: 127.02891,
+        imageUrl: "https://example.com/images/chinese_detail.jpg",
+        restSoloLevel: 2,
+        representativeMenu: "짜장면, 짬뽕, 볶음밥",
+        restaurantDetail: "안암역에서 걸어가기 좋은 1인 식사 가능 중식당",
+        locationInfo: {
+          stationId: 639,
+          stationName: "안암역",
+          distance: 65,
+          time: 1,
         },
-        {
-          "featId": 3,
-          "featName": "키오스크 주문"
-        }
-      ],
-      "restReviewTagList": [
-        {
-          "tagId": 1,
-          "tagName": "가성비"
-        },
-        {
-          "tagId": 2,
-          "tagName": "빠른 식사"
-        },
-        {
-          "tagId": 5,
-          "tagName": "혼밥 추천"
-        }
-      ]
-    },
-    {
-      "restaurantId": 3,
-      "name": "홍콩반점 강남점",
-      "imageUrl": "https://example.com/images/chinese_detail.jpg",
-      "restSoloLevel": 2,
-      "representativeMenu": "짜장면, 짬뽕",
-      "restaurantDetail": "혼자서도 부담 없이 방문 가능한 중식당",
-      "locationInfo": {
-        "stationId": 1,
-        "stationName": "강남역",
-        "distance": 400,
-        "time": 5
+        restFeatureList: [
+          {
+            featId: 2,
+            featName: "1인 메뉴 가능",
+          },
+          {
+            featId: 4,
+            featName: "포장 가능",
+          },
+        ],
+        restReviewTagList: [
+          {
+            tagId: 6,
+            tagName: "든든함",
+          },
+          {
+            tagId: 2,
+            tagName: "빠른 식사",
+          },
+          {
+            tagId: 4,
+            tagName: "점심 추천",
+          },
+        ],
       },
-      "restFeatureList": [
-        {
-          "featId": 2,
-          "featName": "1인 메뉴 가능"
+      {
+        restaurantId: 4,
+        name: "안암 샐러드",
+        lat: 37.58682,
+        lng: 127.02854,
+        imageUrl: "https://example.com/images/salady_detail.jpg",
+        restSoloLevel: 1,
+        representativeMenu: "닭가슴살 샐러드, 연어 포케",
+        restaurantDetail: "가볍게 혼밥하고 싶을 때 들르기 좋은 샐러드집",
+        locationInfo: {
+          stationId: 639,
+          stationName: "안암역",
+          distance: 80,
+          time: 2,
         },
-        {
-          "featId": 4,
-          "featName": "포장 가능"
-        }
-      ],
-      "restReviewTagList": [
-        {
-          "tagId": 6,
-          "tagName": "매운맛"
-        },
-        {
-          "tagId": 2,
-          "tagName": "빠른 식사"
-        },
-        {
-          "tagId": 4,
-          "tagName": "웨이팅 적음"
-        }
-      ]
-    },
-    {
-      "restaurantId": 4,
-      "name": "샐러디 강남역점",
-      "imageUrl": "https://example.com/images/salady_detail.jpg",
-      "restSoloLevel": 1,
-      "representativeMenu": "닭가슴살 샐러드, 연어 포케",
-      "restaurantDetail": "건강식을 선호하는 혼밥러에게 적합한 샐러드 전문점",
-      "locationInfo": {
-        "stationId": 1,
-        "stationName": "강남역",
-        "distance": 180,
-        "time": 2
+        restFeatureList: [
+          {
+            featId: 1,
+            featName: "1인석 있음",
+          },
+          {
+            featId: 5,
+            featName: "콘센트 있음",
+          },
+        ],
+        restReviewTagList: [
+          {
+            tagId: 7,
+            tagName: "건강식",
+          },
+          {
+            tagId: 5,
+            tagName: "혼밥 추천",
+          },
+          {
+            tagId: 8,
+            tagName: "깔끔함",
+          },
+        ],
       },
-      "restFeatureList": [
-        {
-          "featId": 1,
-          "featName": "1인석 있음"
+      {
+        restaurantId: 5,
+        name: "안암 김밥천국",
+        lat: 37.58551,
+        lng: 127.02952,
+        imageUrl: "https://example.com/images/gimbap_detail.jpg",
+        restSoloLevel: 1,
+        representativeMenu: "참치김밥, 제육덮밥",
+        restaurantDetail: "메뉴가 다양해서 혼자 먹기 편한 안암역 분식집",
+        locationInfo: {
+          stationId: 639,
+          stationName: "안암역",
+          distance: 95,
+          time: 2,
         },
-        {
-          "featId": 5,
-          "featName": "콘센트 제공"
-        }
-      ],
-      "restReviewTagList": [
-        {
-          "tagId": 7,
-          "tagName": "건강식"
-        },
-        {
-          "tagId": 5,
-          "tagName": "혼밥 추천"
-        },
-        {
-          "tagId": 8,
-          "tagName": "깔끔함"
-        }
-      ]
-    },
-    {
-      "restaurantId": 5,
-      "name": "김밥천국 강남점",
-      "imageUrl": "https://example.com/images/gimbap_detail.jpg",
-      "restSoloLevel": 1,
-      "representativeMenu": "참치김밥, 제육덮밥",
-      "restaurantDetail": "메뉴가 다양하고 혼밥하기 편한 분식집",
-      "locationInfo": {
-        "stationId": 1,
-        "stationName": "강남역",
-        "distance": 320,
-        "time": 4
+        restFeatureList: [
+          {
+            featId: 2,
+            featName: "1인 메뉴 가능",
+          },
+          {
+            featId: 4,
+            featName: "포장 가능",
+          },
+        ],
+        restReviewTagList: [
+          {
+            tagId: 1,
+            tagName: "가성비",
+          },
+          {
+            tagId: 3,
+            tagName: "한식 가능",
+          },
+          {
+            tagId: 5,
+            tagName: "혼밥 추천",
+          },
+        ],
       },
-      "restFeatureList": [
-        {
-          "featId": 2,
-          "featName": "1인 메뉴 가능"
-        },
-        {
-          "featId": 4,
-          "featName": "포장 가능"
-        }
-      ],
-      "restReviewTagList": [
-        {
-          "tagId": 1,
-          "tagName": "가성비"
-        },
-        {
-          "tagId": 3,
-          "tagName": "야식 가능"
-        },
-        {
-          "tagId": 5,
-          "tagName": "혼밥 추천"
-        }
-      ]
-    }
-  ]
-},
+    ],
+  },
 ];
---
+++ honbabmap/src/pages/Result.jsx
@@ -0,0 +1,14 @@
+import ResultImg from "../assets/eatbti1_result5.png";
+import "../styles/Result.css";
+
+function Result() {
+  return (
+    <div className="container">
+      <div className="result">
+        <img src={ResultImg} alt="result" />
+      </div>
+    </div>
+  );
+}
+export default Result;
+      
